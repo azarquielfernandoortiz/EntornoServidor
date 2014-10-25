9bf23c7ca4367127359f1d7fceccef41efc25ef5
Ortiz <Ortiz@Ortiz-PC>
2014-10-25 12:46:53 +0200

    Primera Confirmación. Trabajamos en local no se subirá al repositorio remoto.

---
+++ TestGitHub/src/controlador/Principa.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import modelo.Saluda;
+
+public class Principa {
+
+	public Principa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Saluda saludo=new Saluda();
+		saludo.diHola();
+		saludo.diAdios();
+		
+
+	}
+
+}
---
+++ TestGitHub/src/modelo/Saluda.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Saluda {
+
+	public Saluda() {
+		// TODO Auto-generated constructor stub
+	}
+	public void diHola(){
+		System.out.println("Hola que tal estás...");
+	}
+	public void diAdios(){
+		System.out.println("Adios, espero verte pronto...");
+	}
+
+}
